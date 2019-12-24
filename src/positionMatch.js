b54bfa44189a14640c7fb0b9058446e344c040d5
YanceyOfficial <yanceyofficial@gmail.com>
2019-12-24 19:09:20 +0800

    feat: learn position match

--- src/positionMatch.js
+++ src/positionMatch.js
@@ -7,3 +7,5 @@ console.log('Merry\nChristmas.'.replace(/^|$/gm, '🎄'))
 console.log('[JS] Lesson_01.mp4'.replace(/\b/g, '#')) // [#JS#] #Lesson_01#.#mp4#
 
 console.log('[JS] Lesson_01.mp4'.replace(/\B/g, '#')) // #[J#S]# L#e#s#s#o#n#_#0#1.m#p#4
+
+console.log('abcde abcdx abxxx'.match(/ab(?=cd)/g))
